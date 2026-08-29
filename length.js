345bc78775f0c033f22e4f425a9ec7337e97fd1b
Al Amin <alamin.dev01@gmail.com>
2026-08-29 23:32:03 +0600

    added some code

--- length.js
+++ length.js
@@ -1,2 +1,8 @@
 const numbers = [12, 22, 54, 66, 77, 33, 44, 456, 543, 123, 4543, 990, 55, 1232, 4321, 4321];
 console.log(numbers.length)
+console.log(numbers[5])
+
+
+numbers[16] = 0
+console.log(numbers[16])
+console.log(numbers)
